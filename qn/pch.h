a893f24dccaf78579b21cc03e47c136201dbce68
ksh local <kshman@msn.com>
2023-11-24 21:30:53 +0900

    시작해볼까

---
+++ qn/pch.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+// Windows
+#if defined(_WIN32)
+#define STRICT
+#define WIN32_LEAN_AND_MEAN
+#include <sdkddkver.h>
+#include <windows.h>
+#include <excpt.h>
+#include <mmsystem.h>
+#if _MSC_VER
+#include <intrin.h>
+#include <crtdbg.h>
+#endif
+#endif
+
+// GCC
+#if __GNUC__
+#define _GNU_SOURCE		1		// GNU 라이브러리
+#include <unistd.h>
+#endif
+
+// comon
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <wchar.h>
+#include <math.h>
+
+//
+#if _MSC_VER
+#pragma warning(disable:4100)		// 참조되지 않은 정식 매개 변수입니다.
+#pragma warning(disable:4127)		// 조건식이 상수입니다.
+#pragma warning(disable:4505)		// 참조되지 않은 지역 함수를 제거했습니다.
+#endif
+
+//
+#if __GNUC__
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wmissing-braces"
+#endif
---
+++ qn/qn.cpp
@@ -0,0 +1,94 @@
+﻿#include "pch.h"
+#include "qn.h"
+
+enum
+{
+	qn_ver_major = 3,
+	qn_ver_minor = 4,
+	qn_ver_build = 0,
+	qn_ver_rev = 0
+};
+
+//
+typedef void (*func_closure_param)(pointer_t);
+
+// 닫아라
+struct Closure
+{
+	struct Closure* prev;
+	funcparam_t		data;
+	bool			zero;
+};
+
+// 구현
+static struct QnRuntime
+{
+	bool			inited;
+	struct Closure* closures;
+	struct Closure* preclosures;
+} _qn_rt = {
+	FALSE,
+	NULL,
+	NULL
+};
+static void qn_dispose(void);
+
+//
+static void qn_init(void)
+{
+	_qn_rt.inited = TRUE;
+
+#if _LIB
+	(void)atexit(qn_dispose);
+#endif
+}
+
+//
+static void qn_dispose(void)
+{
+	if (!_qn_rt.inited)
+		return;
+
+	struct Closure* node;
+	struct Closure* prev;
+
+	for (node = _qn_rt.closures; node; node = prev)
+	{
+		prev = node->prev;
+		if (node->zero)
+			node->data.func();
+		else
+			((func_closure_param)node->data.func)(node->data.data);
+		free(node);
+	}
+
+	for (node = _qn_rt.preclosures; node; node = prev)
+	{
+		prev = node->prev;
+		((func_closure_param)node->data.func)(node->data.data);
+		free(node);
+	}
+}
+
+//
+void qn_runtime(int32_t v[])
+{
+#if _LIB
+	if (!_qn_rt.inited)
+		qn_init();
+#endif
+
+	if (v)
+	{
+		v[0] = qn_ver_major;
+		v[1] = qn_ver_minor;
+		v[2] = qn_ver_build;
+		v[3] = qn_ver_rev;
+	}
+}
+
+//
+_Noreturn void qn_exit(const int exitcode)
+{
+	exit(exitcode);
+}
